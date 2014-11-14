a3bded68184365c8136391c85b13dcebc050d909
Alistair Irvine <alistair.irvine@rothamsted.ac.uk>
2014-11-14 00:05:46 +0000

    added additional comments

--- parse_3pt6_spreadsheet.pl
+++ parse_3pt6_spreadsheet.pl
@@ -1,18 +1,11 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use DBI; # load perl postgresql module
 
-# load perl postgresql module
-use DBI;
+use phibase_subroutines qw(connect_to_phibase query_uniprot); # load PHI-base functions
 
-# define credentials for phibase database
-my $db_name = "phibase";
-my $db_user = "postgres";
-my $db_host = "localhost";
-my $db_pw = "Jake0001"; 
-
-# connect to database
-my $db_conn = DBI->connect("DBI:Pg:dbname=$db_name;host=$db_host","$db_user","$db_pw");
+my $db_conn = connect_to_phibase(); # connect to PHI-base database
 
 # parse text file that maps columns headings of the spreadsheet to database field names
 # saving the column name and db field name as key/value pairs in a hash
@@ -21,7 +14,7 @@ open (COL_NAMES_FILE, "column2accession.txt") || die "Error opening input file\n
 # hash to map spreadsheet headings to PHI-base3 db field names
 my %column_mapping;
 
-# each row of the file contains a spreadsheet heading the corresponding db field name, separated by tab
+# each row of the file contains a "spreadsheet heading" and the corresponding "db field name", separated by tab
 # separate these fields and save as key/value pairs in a hash
 # where key becomes the spreadsheet heading & value becomes db field name
 while (<COL_NAMES_FILE>) {
@@ -35,7 +28,10 @@ while (<COL_NAMES_FILE>) {
 close (COL_NAMES_FILE);
 
 # open the tab separated values (TSV) version of the PHI-base spreadsheet
-open (TSV_FILE, "phi-base-1_vs36.txt") || die "Error opening input file\n";
+my $phibase_tsv_filename = 'phi-base-1_vs36.txt';
+open (TSV_FILE, $phibase_tsv_filename) || die "Error opening input file\n";
+print "Processing PHI-base data from $phibase_tsv_filename...\n";
+print "Inserting data for valid Fusarium graminearum annotations into PHI-base v5 database...\n";
 
 # first line gives the spreadsheet column headings
 chomp(my $col_header_line = <TSV_FILE>);
@@ -54,8 +50,11 @@ foreach my $header (@col_headers) {
    push(@new_col_headers,$header) if $header;
 }
 
-# create hash to store all data
-my %phi_base_data;
+# hash to store all annotations with a valid PHI-base accession
+my %valid_phibase_data;
+# array to store invalid PHI-base accessions
+my @invalid_phibase_acc;
+
 # create another hash for the subset with only required fields
 my %required_fields_data;
 # create another hash for fusarium graminearum data
@@ -67,26 +66,34 @@ my $interaction_num = 0;
 # go through each of the remaining lines of the TSV file (each representing a single annotation)
 # save the values of each column to the approriate output file
 while (<TSV_FILE>) {
+
    # each value is separated based on the tab, then saved as an element of the array
    my @phi_array = split(/\t/,$_);
-   my $i=0;
-   # iterate through each column of the annotation, saving it to the appropriate text file
 
-   #create separate hash to store all values
+   # initiate column iterator
+   my $column_num=0;
+
+   # hash to store all values of the current annotation
    my %phi_base_annotation;
 
-   # the name of the text file is determined by mapping the column header to db field name, using the hash created earlier
+   # iterate through each column of the annotation, saving its value to the appropriate text file
+   # the name of the text file is determined by mapping the column header to db field name
    foreach my $phi_value (@phi_array) {
+
       # add data to output file for the individual column
-      open (COLUMN_FILE, ">> ./output/column_".$column_mapping{$new_col_headers[$i]}.".txt") or die "Error opening output file\n";
+      open (COLUMN_FILE, ">> ./output/column_".$column_mapping{$new_col_headers[$column_num]}.".txt")
+         or die "Error opening output file\n";
       print COLUMN_FILE "$phi_value\n";
 
       # add data to the annotation hash
-      $phi_base_annotation{$column_mapping{$new_col_headers[$i]}} = $phi_value;
+      $phi_base_annotation{$column_mapping{$new_col_headers[$column_num]}} = $phi_value;
 
-      $i++;
-      last if ($i == @new_col_headers);  # values after this are just blank, so exit out of foreach loop
-   } # end foreach annotation
+      # increment the column number
+      $column_num++;
+
+      last if ($column_num == @new_col_headers);  # values after this are just blank, so exit out of foreach loop
+
+   } # end foreach column of the annotation
 
    # variable for PHI-base accession number
    my $phi_acc_num;
@@ -97,7 +104,7 @@ while (<TSV_FILE>) {
    if ($phi_acc_num = $phi_base_annotation{"phi_base_acc"} and $phi_acc_num =~ /^PHI:/) {
 
      $phi_acc_num =~ s/PHI://;
-     $phi_base_data{$phi_acc_num} = {%phi_base_annotation};
+     $valid_phibase_data{$phi_acc_num} = {%phi_base_annotation};
 
      # get subset of the hash containing only the required fields
      my @required_fields = (
@@ -134,6 +141,7 @@ while (<TSV_FILE>) {
           and lc $required_fields_annot{"literature_source"} eq "pubmed"
           and $required_fields_annot{"patho_tax"} == 5518 ) {
 
+        # add the required fields of the current annotation to the fusarium hash
         $fusarium_gram_data{$phi_acc_num} = {%required_fields_annot};
 
 	#print "PHI-base Accession:$required_fields_annot{'phi_base_acc'}\n";
@@ -143,7 +151,7 @@ while (<TSV_FILE>) {
 	#print "Host Species NCBI Taxon ID:$required_fields_annot{'host_tax'}\n";
 	#print "PubMed ID:$required_fields_annot{'literature_id'}\n\n";
 
-	# test insert statement
+	# insert data into the pathogen_gene table
 	my $sql_statement2 = qq(INSERT INTO pathogen_gene (ncbi_taxon_id,gene_name) 
 			        SELECT '$required_fields_annot{"patho_tax"}','$required_fields_annot{"gene_name"}'
                                 WHERE NOT EXISTS (
@@ -155,6 +163,7 @@ while (<TSV_FILE>) {
 	my $sql_result2 = $db_conn->prepare($sql_statement2);
 	$sql_result2->execute() or die $DBI::errstr;
 
+	# get the unique identifier for the inserted pathogen_gene record
         my $sql_statement4 = qq(SELECT id FROM pathogen_gene
                                 WHERE ncbi_taxon_id = '$required_fields_annot{"patho_tax"}'
                                 AND gene_name = '$required_fields_annot{"gene_name"}');
@@ -296,7 +305,10 @@ while (<TSV_FILE>) {
 
      } # end if pathogen id = fusarium gram 
 
-   }  # end if PHI-base accession exists
+   } else { # else PHI-base accession does not exist, or is not valid
+     # add the PHI-base accession string to the invalid accession array
+     push(@invalid_phibase_acc,$phi_base_annotation{"phi_base_acc"});
+   }
 
 } # end of file
 close (TSV_FILE);
@@ -304,19 +316,29 @@ close (TSV_FILE);
 # save all of the valid phibase data to file (sorted by PHI-base accession)
 # formatted with the PHI-base accession as the first row
 # then a separate row for each column heading and value, separated by tab
-# with a blank line between each PHI-base annotation
-open (ALL_DATA_FILE, "> ./output/all_phibase_data.tsv") or die "Error opening output file\n";
-foreach my $phi_base_ann (sort {$a<=>$b} keys %phi_base_data) {
+# with a blank line between each PHI-base annotationi
+my $all_data_filename = './output/all_phibase_data.txt';
+open (ALL_DATA_FILE, "> $all_data_filename") or die "Error opening output file\n";
+foreach my $phi_base_ann (sort {$a<=>$b} keys %valid_phibase_data) {
    print ALL_DATA_FILE "PHI:$phi_base_ann\n";
-   foreach my $col_name (sort keys %{ $phi_base_data{$phi_base_ann} }) {
-     print ALL_DATA_FILE "$col_name\t$phi_base_data{$phi_base_ann}{$col_name}\n";
+   foreach my $col_name (sort keys %{ $valid_phibase_data{$phi_base_ann} }) {
+     print ALL_DATA_FILE "$col_name\t$valid_phibase_data{$phi_base_ann}{$col_name}\n";
    }
    print ALL_DATA_FILE "\n";
 }
 close (ALL_DATA_FILE);
 
-# save required data to a separate file, using same format as above
-open (REQUIRED_FIELDS_FILE, "> ./output/required_phibase_data.tsv") or die "Error opening output file\n";
+# save the invalid PHI-base accessions to a separate file
+my $invalid_accessions_filename = './output/invalid_phibase_accessions.txt';
+open (INVALID_FILE, "> $invalid_accessions_filename") or die "Error opening output file\n";
+foreach my $invalid_accession (@invalid_phibase_acc) {
+   print INVALID_FILE "$invalid_accession\n" if defined $invalid_accession;
+}
+close (INVALID_FILE);
+
+# save required data to a separate file
+my $required_data_filename = './output/required_phibase_data.txt';
+open (REQUIRED_FIELDS_FILE, "> $required_data_filename") or die "Error opening output file\n";
 foreach my $phi_base_ann (sort {$a<=>$b} keys %required_fields_data) {
    print REQUIRED_FIELDS_FILE "PHI:$phi_base_ann\n";
    foreach my $col_name (sort keys %{ $required_fields_data{$phi_base_ann} }) {
@@ -329,9 +351,9 @@ foreach my $phi_base_ann (sort {$a<=>$b} keys %required_fields_data) {
 }
 close (REQUIRED_FIELDS_FILE);
 
-# save required data to a separate file, using same format as above
-open (SPECIES_FILE, "> ./output/species_fusarium_gram_data.tsv") or die "Error opening output file\n";
-my $interaction_counter = 0;
+# save all the fusarium gram data to file, using same format as above
+my $fus_gram_filename = './output/species_fusarium_gram_data.txt';
+open (SPECIES_FILE, "> $fus_gram_filename") or die "Error opening output file\n";
 foreach my $phi_base_ann (sort {$a<=>$b} keys %fusarium_gram_data) {
    print SPECIES_FILE "PHI:$phi_base_ann\n";
    foreach my $col_name (sort keys %{ $fusarium_gram_data{$phi_base_ann} }) {
@@ -341,40 +363,50 @@ foreach my $phi_base_ann (sort {$a<=>$b} keys %fusarium_gram_data) {
      }
    }
    print SPECIES_FILE "\n";
-   $interaction_counter++;
 }
 close (SPECIES_FILE);
-print "Total valid interactions for Fusarium gram: $interaction_counter\n";
 
-
-my $sql_query = qq(SELECT * FROM interaction, obsolete_reference, interaction_pathogen_gene_mutant, pathogen_gene_mutant, pathogen_gene, interaction_literature, interaction_host
+# retrieve all of the data inserted into the relevant tables of phibase
+# and save the file in a tab-delimited file, with appropriate headings
+my $sql_query = qq(SELECT * FROM interaction, obsolete_reference, interaction_pathogen_gene_mutant, 
+                     pathogen_gene_mutant, pathogen_gene, interaction_literature, interaction_host
                    WHERE interaction.phi_base_accession = obsolete_reference.phi_base_accession 
-                   AND interaction.id = interaction_pathogen_gene_mutant.interaction_id
-                   AND pathogen_gene_mutant.id = interaction_pathogen_gene_mutant.pathogen_gene_mutant_id
-                   AND pathogen_gene.id = pathogen_gene_mutant.pathogen_gene_id
-                   AND interaction.id = interaction_literature.interaction_id
-                   AND interaction.id = interaction_host.interaction_id
+                     AND interaction.id = interaction_pathogen_gene_mutant.interaction_id
+                     AND pathogen_gene_mutant.id = interaction_pathogen_gene_mutant.pathogen_gene_mutant_id
+                     AND pathogen_gene.id = pathogen_gene_mutant.pathogen_gene_id
+                     AND interaction.id = interaction_literature.interaction_id
+                     AND interaction.id = interaction_host.interaction_id
                  ;);
 my $sql_stmt = $db_conn->prepare($sql_query);
 my $sql_result = $sql_stmt->execute() or die $DBI::errstr;
-my $row_count = 0; 
-  
-open (DATABASE_DATA_FILE, "> ./output/database_data.tsv") or die "Error opening output file\n";
+
+my $annotation_count = 0; 
+
+my $db_data_filename = './output/database_data.tsv';  
+open (DATABASE_DATA_FILE, "> $db_data_filename") or die "Error opening output file\n";
 
 print DATABASE_DATA_FILE "int id\tPHI-base acc\tcuration date\tobsolete ref id\tobsolete ref new accession\tobsolete ref old accession\tint_path_gene_mut int_id\tint_path_gene_mut path_gene_id\tpath_gene_mutant_id\tpath_gene_mutant path_gene_id\tpath_gene_mutant ncbi_taxon_id\tuniprot_accession\tpath_gene id\tpath_gene ncbi_taxon_id\tpath_gene gene_name\tint_lit int_id\tint_lit PubMed ID\tint_host id\tint_host int_id\tint_host ncbi_taxon_id\n";
 
 while (my @row = $sql_stmt->fetchrow_array()) {
-  $row_count++;
-  my $col_count = 0;
+  $annotation_count++;
   foreach my $column (@row) {
-     $col_count++;
      print DATABASE_DATA_FILE "$column\t" if defined $column; 
   }
   print DATABASE_DATA_FILE "\n";
 }
-print "Total interactions:$row_count\n";
 close (DATABASE_DATA_FILE);
 
 $sql_stmt->finish() or die "Failed to finish SQL statement\n";
 $db_conn->disconnect() or die "Failed to disconnect database\n";
+
+print "\nProcess completed successfully.\n\n";
+print "Total PHI-base annotations with valid data: ".scalar(keys %valid_phibase_data)."\n";
+print "Total PHI-base annotations with an invalid accession: ".scalar(@invalid_phibase_acc)."\n";
+print "Total valid interactions for Fusarium graminearum: ".scalar(keys %fusarium_gram_data)."\n";
+print "Total annotations retrieved from database: $annotation_count\n\n";
+print "Output file of all PHI-base annotations with valid data: $all_data_filename\n";
+print "Output file of accession string with an invalid PHI-base accession (if available): $invalid_accessions_filename\n";
+print "Output file of only the required data of valid PHI-base annotations: $required_data_filename\n";
+print "Output file of valid data from fusarium graminearum: $fus_gram_filename\n";
+print "Tab-separated file of all PHI-base data inserted into relevant tables: $db_data_filename\n\n";
 
