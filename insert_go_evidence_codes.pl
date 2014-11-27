eb5916bdcfc18ab202a2d9eb09af97f47584e9e8
Alistair Irvine <alistair.irvine@rothamsted.ac.uk>
2014-11-27 19:09:26 +0000

    removed serial id for go_evidence_code, code now PK

--- insert_go_evidence_codes.pl
+++ insert_go_evidence_codes.pl
@@ -10,19 +10,19 @@ my $db_conn = connect_to_phibase(); # connect to PHI-base database
 # hard-code insert of valid Gene Ontology Evidence Codes
 # for more information, see the GO Evidence Code Decision Tree
 # at http://geneontology.org/page/evidence-code-decision-tree
-my $sql_statement = qq(INSERT INTO go_evidence_code (code) VALUES ('IMP'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IGI'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IPI'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IDA'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IEP'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IEA'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('ISS'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IGC'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('ICA'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('TAS'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('NAS'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('IC'); 
-                       INSERT INTO go_evidence_code (code) VALUES ('ND'); 
+my $sql_statement = qq(INSERT INTO go_evidence (code) VALUES ('IMP'); 
+                       INSERT INTO go_evidence (code) VALUES ('IGI'); 
+                       INSERT INTO go_evidence (code) VALUES ('IPI'); 
+                       INSERT INTO go_evidence (code) VALUES ('IDA'); 
+                       INSERT INTO go_evidence (code) VALUES ('IEP'); 
+                       INSERT INTO go_evidence (code) VALUES ('IEA'); 
+                       INSERT INTO go_evidence (code) VALUES ('ISS'); 
+                       INSERT INTO go_evidence (code) VALUES ('IGC'); 
+                       INSERT INTO go_evidence (code) VALUES ('ICA'); 
+                       INSERT INTO go_evidence (code) VALUES ('TAS'); 
+                       INSERT INTO go_evidence (code) VALUES ('NAS'); 
+                       INSERT INTO go_evidence (code) VALUES ('IC'); 
+                       INSERT INTO go_evidence (code) VALUES ('ND'); 
 		    );
 
 my $sql_result = $db_conn->do($sql_statement) or die $DBI::errstr;
