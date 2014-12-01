829806980d23dd48a8a11b5ad23a3c2188513c17
Alistair Irvine <alistair.irvine@rothamsted.ac.uk>
2014-12-01 20:56:23 +0000

    added category and description to go_evidence table

--- insert_go_evidence_codes.pl
+++ insert_go_evidence_codes.pl
@@ -8,21 +8,44 @@ use phibase_subroutines qw(connect_to_phibase query_uniprot); # load PHI-base fu
 my $db_conn = connect_to_phibase(); # connect to PHI-base database
 
 # hard-code insert of valid Gene Ontology Evidence Codes
-# for more information, see the GO Evidence Code Decision Tree
-# at http://geneontology.org/page/evidence-code-decision-tree
-my $sql_statement = qq(INSERT INTO go_evidence (code) VALUES ('IMP'); 
-                       INSERT INTO go_evidence (code) VALUES ('IGI'); 
-                       INSERT INTO go_evidence (code) VALUES ('IPI'); 
-                       INSERT INTO go_evidence (code) VALUES ('IDA'); 
-                       INSERT INTO go_evidence (code) VALUES ('IEP'); 
-                       INSERT INTO go_evidence (code) VALUES ('IEA'); 
-                       INSERT INTO go_evidence (code) VALUES ('ISS'); 
-                       INSERT INTO go_evidence (code) VALUES ('IGC'); 
-                       INSERT INTO go_evidence (code) VALUES ('ICA'); 
-                       INSERT INTO go_evidence (code) VALUES ('TAS'); 
-                       INSERT INTO go_evidence (code) VALUES ('NAS'); 
-                       INSERT INTO go_evidence (code) VALUES ('IC'); 
-                       INSERT INTO go_evidence (code) VALUES ('ND'); 
+#
+# for more information, see 
+# http://geneontology.org/page/guide-go-evidence-codes
+# and the GO Evidence Code Decision Tree at 
+# http://geneontology.org/page/evidence-code-decision-tree
+#
+# the list below are separated based on the following categories:
+# 1. Experimental evidence codes
+# 2. Computational evidece codes
+# 3. Author statement evidence codes
+# 4. Curational statement evidence codes
+# 5. Automatically-assigned evidence codes (may not be used in PHI-base)
+#
+my $sql_statement = qq(INSERT INTO go_evidence (code,category,description) VALUES ('EXP','Experimental','Inferred from experiment');
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IMP','Experimental','Inferred from mutant phenotype'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IGI','Experimental','Inferred from genetic interaction'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IPI','Experimental','Inferred from physical interaction'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IDA','Experimental','Inferred from direct assay'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IEP','Experimental','Inferred from expression pattern');
+ 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('ISS','Computational','Inferred from sequence or structure similarity'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('ISA','Computational','Inferred from sequence alignment'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('ISO','Computational','Inferred from sequence orthology'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('ISM','Computational','Inferred from sequence model'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IGC','Computational','Inferred from genomic context'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IBA','Computational','Inferred from biological aspect of ancestor'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IBD','Computational','Inferred from biological aspect of descendant'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IKR','Computational','Inferred from key residues'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IRD','Computational','Inferred from rapid divergence'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('RCA','Computational','Inferred from reviewed computational analysis');
+
+                       INSERT INTO go_evidence (code,category,description) VALUES ('TAS','Author statement','Traceable author statement'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('NAS','Author statement','Non-traceable author statement');
+
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IC','Curatorial statement','Inferred by curator'); 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('ND','Curatorial statement','No biological data available');
+ 
+                       INSERT INTO go_evidence (code,category,description) VALUES ('IEA','Automatically-assigned','Inferred from electronic annotation'); 
 		    );
 
 my $sql_result = $db_conn->do($sql_statement) or die $DBI::errstr;
